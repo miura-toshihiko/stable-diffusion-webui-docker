368a0e2fc0df09dae47de0ebb29c09e91fd6d6db
miura-toshihiko <miura.toshihiko@isid.co.jp>
2023-12-24 21:22:41 +0900

    稼働用定義をコミット

---
+++ start.bat
@@ -0,0 +1,7 @@
+cd F:\webui\stable-diffusion-webui-docker2
+f:
+docker compose --profile auto up --build
+
+REM docker compose --profile download up --build
+REM docker compose --profile auto up --build
+
